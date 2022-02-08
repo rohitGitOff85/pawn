e5eabd0be8da60c6731a9dc02927635f2f09e0b0
rohitGitOff85 <99080083+rohitGitOff85@users.noreply.github.com>
2022-02-08 14:45:59 +0530

    Add files via upload

---
+++ pawn.rb
@@ -0,0 +1,21 @@
+class Pawn < ActiveRecord::Base
+  def self.init
+    PawnSimulator.new
+  end
+  def self.process_comments(pawn_simulator_obj, type, options=nil)
+    case type
+    when 'place'
+      pawn_simulator_obj.place(options[:x], options[:y], options[:f], options[:c])
+    when 'move'
+      no_of_step = options[:no_of_step].present? ? options[:no_of_step].to_i : 1
+      pawn_simulator_obj.move(no_of_step)
+    when 'left'
+      pawn_simulator_obj.left
+    when 'right'
+      pawn_simulator_obj.right
+    when 'report'
+      pawn_simulator_obj.report
+    end
+  end
+end
+
